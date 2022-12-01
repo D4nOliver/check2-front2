abb89310fd058ec94018ea3c09a4c73dbff73b3e
unknown <danieloliversilva9@gmail.com>
2022-11-30 21:10:31 -0300

    index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,51 @@
-import { validacao } from "./utils.js";
-
 let email = document.getElementById("inputEmail");
 let password = document.getElementById("inputPassword");
 let submit = document.getElementById("inputSubmit");
-//console.log(email);
 let login = [];
+
 submit.addEventListener("click", (e) => {
   e.preventDefault();
-  let emailTratado = email.value.trim();
-  let senhaTratado = password.value.trim();
+  // tratando os dados
+  let emailLogin = email.value.trim();
+  let senhaLogin = password.value.trim();
+  // add no array login
+  login.push(emailLogin, senhaLogin);
+  //tranfomando em JSON
+  let loginJSON = JSON.stringify(login);
+  //
+  logandoApi(loginJSON);
 });
-//
+//tem q arrumar aki, n ta logando
+function logandoApi(loginJson) {
+  let config = {
+    method: "POST",
+    body: loginJson,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  fetch("https://ctd-fe2-todo-v2.herokuapp.com/v1/users/login", config)
+    .then((resultado) => {
+      if (resultado.status == 201) {
+        return resultado.json();
+      } else {
+        throw resultado;
+      }
+    })
+    .then((resultado) => {
+      loginSucesso(resultado);
+    })
+    .catch((erro) => {
+      loginErro(erro);
+    });
+}
+
+function loginSucesso(resposta) {
+  console.log(resposta);
+}
+function loginErro(resposta) {
+  console.log(resposta);
+}
 email.addEventListener("keyup", () => {
   login = [email.value, password.value];
   validacao(login);
--- scripts/signup.js
+++ scripts/signup.js
@@ -1,31 +1,30 @@
-import { validacao } from "./utils"
+import { validacao } from "./utils";
 
-let name = document.getElementById("name")
-let lastName = document.getElementById("lastName")
-let email = document.getElementById("email")
-let password = document.getElementById("password")
-let password2 = document.getElementById("password2")
-let submit = document.getElementById("submit")
+let name = document.getElementById("name");
+let lastName = document.getElementById("lastName");
+let email = document.getElementById("email");
+let password = document.getElementById("password");
+let password2 = document.getElementById("password2");
+let submit = document.getElementById("submit");
 
-submit.addEventListener("click", (e)=>{
-    e.preventDefault()
-    //preventDefault parou de funcionar
-    if(validacao()){
-        name = name.value.trim()
-        lastName = lastname.value.trim()
-        email = email.value.trim()
-        password = password.value.trim()
-        password2 = password2.value.trim()
-        //consegui tratar os dados mas depois de usar a validacao() ainda nao funcionou
-        let signupJs = {
-            "firstName": name,
-            "lastName": lastName,
-            "email": email,
-            "password": password
-        }
-        
-        let signupJson = JSON.stringify(signupJs)
-        console.log(signupJson)
-    }
-   
-})
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+  //preventDefault parou de funcionar
+  if (validacao()) {
+    name = name.value.trim();
+    lastName = lastname.value.trim();
+    email = email.value.trim();
+    password = password.value.trim();
+    password2 = password2.value.trim();
+    //consegui tratar os dados mas depois de usar a validacao() ainda nao funcionou
+    let signupJs = {
+      firstName: name,
+      lastName: lastName,
+      email: email,
+      password: password,
+    };
+
+    let signupJson = JSON.stringify(signupJs);
+    console.log(signupJson);
+  }
+});
